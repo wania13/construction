7101511cc615a9b25b4724178094c30d08d35501
wania13 <waniakanwal98@gmail.com>
2020-09-02 20:16:39 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+
+
+
+
+      $('.slider-content').slick({
+
+        nextArrow: "",
+          prevArrow: "",
+          autoplay:'true',
+      infinite: true,
+      speed: 200,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            // dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+});
+
+
+
+// $("#myModal").on('hidden.bs.modal', function (e) {
+//     $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+
+
+$(document).ready(function(){
+  $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+    $(this).toggleClass('open');
+    $('.tohide').toggleClass('show');
+  });
+});
